53f355f0c34066a98e486dd67068c16ec659ab08
williamycsiu <wsiu4@hawaii.edu>
2016-10-11 23:05:01 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,1 +1,32 @@
-console.log("Hello world");
+PlayersList = new Mongo.Collection('players');
+
+var playersList = ['David', 'Bob', 'Mary', 'Bill', 'Warren', 'Tim'];
+playersList.forEach(function () {
+  console.log('test');
+});
+
+if (Meteor.isClient) {
+  console.log("Hello client");
+  Template.leaderboard.helpers({
+    'player': function () {
+      return PlayersList.find();
+    },
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected"
+      }
+    }
+  });
+  Template.leaderboard.events({
+    'click .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  console.log("Hello server");
+}
